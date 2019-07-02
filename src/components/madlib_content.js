610dcde52b9108577b997cb36cb140d8e4e14cf8
Neral <christian.neral@708n-j218-cn-01.local>
2019-07-02 17:28:20 -0600

    fixed typos

--- src/components/madlib_content.js
+++ src/components/madlib_content.js
@@ -15,13 +15,13 @@ class MadLibContent extends Component { //defines React component, props establi
                 Maybe, more like <label className="black-label">8</label>{this.renderLabel(data.numTwo)}. 
 
                 Where would this person go to find the <label className="black-label">9</label>{this.renderLabel(data.pluralNounTwo)}? <label className="black-label">2</label>{this.renderLabel(data.celebOne)} would have to <label className="black-label">10</label>{this.renderLabel(data.verbOne)} <label className="black-label">11</label>{this.renderLabel(data.adverbOne)} in 
-                order to find true <label className="black-label">12</label>{this.renderLabel(data.nounTwo)} again. Just as the everything seemed hopeless, out from the palm trees
+                order to find true <label className="black-label">12</label>{this.renderLabel(data.nounTwo)} again. Just as everything seemed hopeless, out from the palm trees
                 came a <label className="black-label">15</label>{this.renderLabel(data.animalOne)}. He simply chose to <label className="black-label">14</label>{this.renderLabel(data.verbTwo)} out from 
                 the palms. <label className="black-label">2</label>{this.renderLabel(data.celebOne)} decided to name <label className="black-label">15</label>{this.renderLabel(data.animalOne)} <label className="black-label">16</label>{this.renderLabel(data.firstNameOne)}. Things seemed 
                 much <label className="black-label">13</label>{this.renderLabel(data.adjThree)}! Yet, when they got to <label className="black-label">2</label>{this.renderLabel(data.celebOne)}'s new <label className="black-label">18</label>{this.renderLabel(data.carBrandOne)}, it 
                 spilled <label className="black-label">2</label>{this.renderLabel(data.celebOne)}'s cup of <label className="black-label">17</label>{this.renderLabel(data.liquidOne)} all over 
                 the interior! <label className="black-label">2</label>{this.renderLabel(data.celebOne)} was <label className="black-label">19</label>{this.renderLabel(data.adjFour)}, and<label className="black-label">20</label> {this.renderLabel(data.adverbTwo)} kicked the <label className="black-label">5</label>{this.renderLabel(data.animalOne)} out 
-                of his car. Still, <label className="black-label">2</label>{this.renderLabel(data.celebOne)} felt better and drove off.
+                of the car. Still, <label className="black-label">2</label>{this.renderLabel(data.celebOne)} felt better and drove off.
                 </div>
             </div>
         )
